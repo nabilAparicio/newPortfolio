b2affb16e454697a2abf78e0e629a0c505932dbd
Orlando Aparicio <orlando.nabil@gmail.com>
2024-07-30 15:01:29 -0500

    setting i18n library

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,20 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-
+import paraglide from "@inlang/paraglide-astro";
 import playformInline from "@playform/inline";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), playformInline()],
+  integrations: [
+    tailwind(),
+    playformInline(),
+    paraglide({
+      project: "./project.inlang",
+      outdir: "./src/paraglide",
+    }),
+  ],
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "es"],
+  },
 });
